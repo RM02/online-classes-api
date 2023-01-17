7d703881b063bb96f3a00e3b202b210b85787166
RM02 <rafamark244@gmail.com>
2023-01-17 11:23:27 -0400

    vercel config

--- infra/express/index.js
+++ infra/express/index.js
@@ -3,11 +3,11 @@ const cors = require("cors");
 
 const app = express()
 const router = require("./router");
-
+const PORT = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json())
 app.use(router)
 
-app.listen(process.env.PORT || 3000, () => console.log(`Running on port ${process.env.PORT}`))
+app.listen(PORT, () => console.log(`Running on port ${PORT}`))
 
 module.exports = app;
